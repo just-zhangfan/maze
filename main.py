d4087d30818e5aff548dc9c18d647f3b031c9643
just-zhangfan <2868842110@qq.com>
2023-11-27 21:18:08 +0800

    Initialize the interface

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import config
+
+
+pygame.init()
+screen = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # 关闭窗口结束
+            running = False
+
+    screen.fill('black')
+    pygame.display.flip()  # 染完色更新一哈
+
+    clock.tick(config.FPS)  # 每秒执行60次循环(帧)，通过while加函数实现60帧
+
+
+pygame.quit()  # 回收资源
