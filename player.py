44d9cfb8b9026b9954c1f936d68ef44906bffc2b
just-zhangfan <2868842110@qq.com>
2023-11-28 14:16:12 +0800

    让小车能够斜着移动

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 import config
+import math
 
 
 class Player(pygame.sprite.Sprite):  # 小组件
@@ -7,8 +8,10 @@ class Player(pygame.sprite.Sprite):  # 小组件
         super(Player, self).__init__()
         self.width = 100
         self.height = 50
+        self.forward_angle = 60  # 转弯的角度（与x轴顺时针转动的角度）
         self.image_source = pygame.image.load('static/images/car.png').convert()
         self.image = pygame.transform.scale(self.image_source, (self.width, self.height))
+        self.image = pygame.transform.rotate(self.image, -self.forward_angle)  # 这里参数是逆时针
         self.image.set_colorkey('black')  # 将小车的黑色变成透明
         self.rect = self.image.get_rect()  # 取出矩形
         self.rect.center = (config.SCREEN_WIDTH / 2, config.SCREEN_HEIGHT / 2)  # 对齐地图中心
@@ -19,7 +22,6 @@ class Player(pygame.sprite.Sprite):  # 小组件
         self.move_velocity = 0  # 当前的移动速度
         self.move_acc = 600  # 每秒将速度增加600
         self.friction = 0.95  # 摩檫力
-
 
     def update_delta_time(self):
         cur_time = pygame.time.get_ticks()
@@ -38,7 +40,12 @@ class Player(pygame.sprite.Sprite):  # 小组件
             self.move_velocity = int(self.move_velocity * self.friction)  # 没按up时，一直*f，直到速度降为0
 
     def move(self):
-        self.rect.x += self.move_velocity * self.delta_time
+        if self.move_velocity != 0:
+            # math.cos要接收弧度，需要将角度转换为弧度(几分之pi形式)
+            vx = self.move_velocity * math.cos(math.pi * self.forward_angle / 180)
+            vy = self.move_velocity * math.sin(math.pi * self.forward_angle / 180)
+            self.rect.x += vx * self.delta_time
+            self.rect.y += vy * self.delta_time
 
     def update(self):
         self.update_delta_time()  # 每次update调用一次更新函数
