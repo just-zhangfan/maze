3dd32968f57c57e4e95de20eff11a9a519ccc7e2
just-zhangfan <2868842110@qq.com>
2023-11-28 14:30:02 +0800

    让小车能够转动起来

--- player.py
+++ player.py
@@ -21,6 +21,8 @@ class Player(pygame.sprite.Sprite):  # 小组件
         self.move_velocity_limit = 220  # 设置移动速度上限
         self.move_velocity = 0  # 当前的移动速度
         self.move_acc = 600  # 每秒将速度增加600
+        self.rotate_velocity_limit = 140  # 旋转速度上限
+        self.rotate_velocity = 0  # 旋转速度（角速度）
         self.friction = 0.95  # 摩檫力
 
     def update_delta_time(self):
@@ -39,8 +41,24 @@ class Player(pygame.sprite.Sprite):  # 小组件
         else:
             self.move_velocity = int(self.move_velocity * self.friction)  # 没按up时，一直*f，直到速度降为0
 
+        if key_pressed[pygame.K_RIGHT]:
+            self.rotate_velocity = self.rotate_velocity_limit
+        elif key_pressed[pygame.K_LEFT]:
+            self.rotate_velocity = -self.rotate_velocity_limit
+        else:
+            self.rotate_velocity = 0  # 没有按的时候角速度清空
+
+    def rotate(self):  # 处理车身的转动
+        self.forward_angle += self.rotate_velocity * self.delta_time
+        # 角度转动后需要改变下图像
+        # 每次都要从原始图像来转，如果用新的转，就会一直叠加，然后一动一下就飞出去了
+        self.image = pygame.transform.scale(self.image_source, (self.width, self.height))
+        self.image = pygame.transform.rotate(self.image, -self.forward_angle)
+        self.image.set_colorkey('black')
+
     def move(self):
         if self.move_velocity != 0:
+            self.rotate()  # 只有车在前进或后退时，车身角度才会变化，原地打方向盘，车身角度不动
             # math.cos要接收弧度，需要将角度转换为弧度(几分之pi形式)
             vx = self.move_velocity * math.cos(math.pi * self.forward_angle / 180)
             vy = self.move_velocity * math.sin(math.pi * self.forward_angle / 180)
