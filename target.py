31ee841bceb80cf0f9f2f380aed1f50610318f32
just-zhangfan <2868842110@qq.com>
2023-11-28 19:45:37 +0800

    星星皇冠呼吸效果

--- target.py
+++ target.py
@@ -9,6 +9,18 @@ class Target(pygame.sprite.Sprite):
         self.image.set_colorkey('black')
         self.rect = self.image.get_rect()
         self.rect.center = (center_x, center_y)
+        self.scale = 1  # 星星大小默认1倍
+        self.scale_delta = 0.01  # 每一帧星星加0.01倍
 
     def update(self):
-        pass
+        self.scale += self.scale_delta
+        # 不能一直变大
+        if self.scale > 1.1 or self.scale < 0.9:
+            self.scale_delta *= -1
+        # 更新（但是是以左上角为锚点来变化的）
+        self.image = pygame.transform.scale(self.image_source, (100 * self.scale, 100 * self.scale))
+        # 存下当前rect的中心点
+        center = self.rect.center
+        # 再求下新image的rect，再将rect的center与center重合
+        self.rect = self.image.get_rect()
+        self.rect.center = center
